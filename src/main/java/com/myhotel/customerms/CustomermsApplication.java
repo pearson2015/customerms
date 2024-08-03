7ce018f68c71f3713737aedfd372162448cceff1
shahin <shahin.mfsb@gmail.com>
2024-08-03 18:10:29 +0530

    docker file added

--- src/main/java/com/myhotel/customerms/CustomermsApplication.java
+++ src/main/java/com/myhotel/customerms/CustomermsApplication.java
@@ -2,8 +2,10 @@ package com.myhotel.customerms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomermsApplication {
 
 	public static void main(String[] args) {
